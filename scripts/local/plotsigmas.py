bd48c7a4916956e93de58c31a214a15dac9fe83a
Sam Kohn <kohn@berkeley.edu>
2016-06-28 16:23:17 -0700

    Re-scale spectrum output to appropriate units
    This involves a product of POT/yr, bin widths, cross section units,
    number of targets, etc.

--- scripts/local/plotsigmas.py
+++ scripts/local/plotsigmas.py
@@ -19,8 +19,16 @@ quantitieslist = [
         sigmas.eff
 ]
 
+pot_per_year = 1.1e21
+years = 3.125
+flux_file_bin_width=0.125
+my_bin_width = 10.0/SimulationComponent.defaultBinning.n
+num_targets = 6e32
+xsec_units = 1e-4
+physicsfactor = pot_per_year * years * my_bin_width/flux_file_bin_width
+physicsfactor *= num_targets * xsec_units
 
-defaultspectrum = (sigmas.flux['default']
+defaultspectrum = physicsfactor * (sigmas.flux['default']
         .evolve(sigmas.oscprob['default'])
         .evolve(sigmas.xsec['default'])
         .evolve(sigmas.drm['default'])
@@ -30,11 +38,11 @@ fig = plt.figure(1)
 for plotnum, (name, quantity_to_adjust) in enumerate(quantities.iteritems()):
     ax = plt.subplot(2, 3, plotnum+1)
     if quantity_to_adjust is sigmas.flux:
-        spectrum_plus = sigmas.flux['+1sigma']
-        spectrum_minus = sigmas.flux['-1sigma']
+        spectrum_plus = physicsfactor * sigmas.flux['+1sigma']
+        spectrum_minus = physicsfactor * sigmas.flux['-1sigma']
     else:
-        spectrum_plus = sigmas.flux['default']
-        spectrum_minus = sigmas.flux['default']
+        spectrum_plus = physicsfactor * sigmas.flux['default']
+        spectrum_minus = physicsfactor * sigmas.flux['default']
     for nextquantity in quantitieslist[1:]:
         if quantity_to_adjust is nextquantity:
             spectrum_plus = spectrum_plus.evolve(
@@ -51,5 +59,10 @@ for plotnum, (name, quantity_to_adjust) in enumerate(quantities.iteritems()):
             'b--')
     flux_minus_im = ax.plot(bins, spectrum_minus.extract('nue'),
             'b:')
+    ax.set_xlim([0, 10])
+    ax.set_ylim([0, 35])
+    ax.set_xlabel("Energy [GeV]")
+    ax.set_ylabel("Neutrino events per 0.083 GeV")
     ax.set_title(name)
+    ax.legend(["Nominal", r"$+1\sigma$", r"$-1\sigma$"])
 plt.show()
